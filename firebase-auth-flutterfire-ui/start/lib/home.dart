1d8ed5620142b7bd8dbf0236428f4f259cf42485
Kush Chakraborty <ks2d85@gmail.com>
2024-09-30 00:51:29 +0530

    this may probably work

--- firebase-auth-flutterfire-ui/start/lib/home.dart
+++ firebase-auth-flutterfire-ui/start/lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,6 +8,39 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<ProfileScreen>(
+                  builder: (context) => ProfileScreen(
+                    appBar: AppBar(
+                      title: const Text('My profile'),
+                    ),
+                    actions: [
+                      SignedOutAction((context) {
+                          Navigator.of(context).pop();
+                        }
+                      ),
+                    ],
+                    children: [
+                      const Divider(),
+                      Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Image.asset('flutterfire_300x.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        ],
         automaticallyImplyLeading: false,
       ),
       body: Center(
@@ -17,6 +51,7 @@ class HomeScreen extends StatelessWidget {
               'Welcome!',
               style: Theme.of(context).textTheme.displaySmall,
             ),
+            const SignOutButton(),
           ],
         ),
       ),
